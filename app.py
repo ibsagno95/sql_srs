5c64a87196e823a42cb1c7c5af3cbb0569d71eb8
ibsagno95 <ibsagno95@gmail.com>
2024-10-17 16:37:55 +0200

    conflits

--- app.py
+++ app.py
@@ -64,11 +64,5 @@ with tab1:
 with tab2:
     st.write(answer)
 
-#data = {"a": [1, 2, 3], "b": [4, 5, 6]}
-#df = pd.DataFrame(data)
 
-#sql_query = st.text_area(label="entrez votre input")
-#result= duckdb.query(sql_query).df()
-#query = st.write(f"vous avez rentrez la requête suivante {sql_query}")
-#st.dataframe(result)
 
